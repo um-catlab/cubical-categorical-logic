e8067468cf66813dcf255254288ee17ec2b80ea2
Steven Schaefer <stschaef@umich.edu>
2024-05-29 17:27:27 -0400

    update gitignore + line length script update

--- check-line-lengths.sh
+++ check-line-lengths.sh
@@ -2,7 +2,32 @@
 # originally written by Steven Schaefer <stschaef>
 
 error=0
+# Bash 4 or 5 to run this
 shopt -s globstar nullglob
+
+VENV_DIR="env"
+
+# Check if the virtual environment directory exists
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Virtual environment does not exist. Creating one now..."
+    # Create the virtual environment with python3
+    python3 -m venv "$VENV_DIR" || { echo "Failed to create virtual environment."; exit 1; }
+
+fi
+
+source "$VENV_DIR/bin/activate"
+
+if [ $? -ne 0 ]; then
+    echo "Error: Unable to activate the virtual environment."
+    exit 2
+fi
+
+echo "Virtual environment activated."
+
+pip3 install wcwidth || { echo "Failed to install wcwidth."; exit 3; }
+
+echo "Package 'wcwidth' has been successfully installed in the virtual environment."
+
 for file in **/*.agda; do
   python3 -c '
 import sys
